762877756be01a076ff85e659fcc068864edd5fc
Dwayne Truex <51888733+Dwaynet86@users.noreply.github.com>
2019-06-30 16:08:07 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# Store all configuration settings here
+
+temperature = null;
+humidity = null;
+
+
+
+db_user = "weathermonitor";
+db_password = "weather";
+db_name = "weather";
+db_table = "readings";

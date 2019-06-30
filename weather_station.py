ad1f7b851d1c2de69babdc4cd8447da4c754062b
Dwayne Truex <51888733+Dwaynet86@users.noreply.github.com>
2019-06-30 15:57:57 -0500

    Update weather_station.py

--- weather_station.py
+++ weather_station.py
@@ -1,1 +1,27 @@
 # Main program for weather monitoring
+
+# Import any needed files
+
+from config import *
+from time import sleep # Import sleep Module for timing
+
+
+# define 
+
+def read_sensors():
+  # read temp
+  
+  # read hum
+  
+def store_readings():
+  # store readings in db for history
+  
+def open_db  
+
+#Main Loop
+while True: # Loop Continuously
+    
+    read_sensors() # poll sensor data
+    store_readings() # store data from sensors
+    
+    sleep(10)

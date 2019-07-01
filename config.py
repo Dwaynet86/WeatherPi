19fbb838adee69edbd821b1d8ddc2bc18412f8e9
Dwayne Truex <51888733+Dwaynet86@users.noreply.github.com>
2019-06-30 20:45:20 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,7 @@
 # Store all configuration settings here
 
-temperature = null;
-humidity = null;
 
-
-
-db_user = "weathermonitor";
-db_password = "weather";
-db_name = "weather";
-db_host = "localhost";
+db_user = "weathermonitor"
+db_password = "weather"
+db_name = "weather"
+db_host = "localhost"

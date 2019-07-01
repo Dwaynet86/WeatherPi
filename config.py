87f1aa2e25aa62263a67bbdde78b5a5cf02d5c08
Dwayne Truex <51888733+Dwaynet86@users.noreply.github.com>
2019-06-30 21:07:38 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
 # Store all configuration settings here
 
+# Database credentials
 
-db_user = "weathermonitor"
+db_user     = "weathermonitor"
 db_password = "weather"
-db_name = "weather"
-db_host = "localhost"
+db_name     = "weather"
+db_host     = "localhost"
+
+
+# User Options
+
+read_interval = 300  # Sensor Read Interval Time in Seconds 300 = 5 minutes

82bf961a751951ab18b120183c712d753d65a3b2
Dwayne Truex <51888733+Dwaynet86@users.noreply.github.com>
2019-06-30 21:29:19 -0500

    Create read_sensors.py

---
+++ read_sensors.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+# Functions for getting weather data from sensors
+
+def read_temperature():
+  # temperature = read.temperature()
+  
+  return
+
+def read_humidity():
+  # humidity = read.humidity()
+  
+  return
+  
+def read_wind():
+  # wind_speed = read.wind()
+  # wind_direction = read.direction()
+  return
+  
+def read_precipitation():
+  # is_raining = read.()
+  # rain_drops = 
+  return
+  
+def read_air_quality():
+  # air_quality  = read.()
+  
+  return

ac61cb879ca7b3efeed62748e735d2557873ee1b
Dwayne Truex <51888733+Dwaynet86@users.noreply.github.com>
2019-07-08 22:20:53 -0500

    Update weather_station.py

--- weather_station.py
+++ weather_station.py
@@ -147,7 +147,7 @@ loop = 0
 while True: # Loop Continuously
     
     #read_sensors() # poll sensor data
-    #store_readings() # store data from sensors
+    store_readings() # store data from sensors
     loop += 1
     if loop == 10: break
     print ("Reading sensors {}".format(loop))

0c2801a0a84ad822ef3c4b4bc358bedea526585b
Dwayne Truex <51888733+Dwaynet86@users.noreply.github.com>
2019-07-10 00:41:52 -0500

    Update read_sensors.py

--- read_sensors.py
+++ read_sensors.py
@@ -12,11 +12,11 @@ def read_temperature():
   pin = 4
   humidity_raw, temperature_raw = Adafruit_DHT.read_retry(sensor, pin)
   if humidity_raw is not None and temperature_raw is not None:
-    # Convert to F from C
-    temperature_new = round(temperature_raw * 1.8 + 32,1)
-    humidity_new = round(humidity_raw,1)
-    print (humidity_raw, humidity_new, temperature_raw, temperature_new)
-    print('Temp={0:0.1f}*F  Humidity={1:0.1f}%'.format(temperature_new, humidity_new))
+    # Convert to F from C and drop excess after decimal
+    temperature_new = round(temperature_raw * 1.8 + 32, 1)
+    humidity_new = round(humidity_raw, 1)
+    
+    print('Temp={0:0.1f}*F  Humidity={1:0.1f}%'.format(temperature_new, humidity_new)) # for demo only
     
     
     

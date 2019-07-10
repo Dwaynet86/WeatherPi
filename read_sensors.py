61e4b8f0a8a612cfd29b0334c66857fd941841a6
Dwayne Truex <51888733+Dwaynet86@users.noreply.github.com>
2019-07-10 00:06:04 -0500

    Update read_sensors.py

--- read_sensors.py
+++ read_sensors.py
@@ -12,6 +12,7 @@ def read_temperature():
   pin = 4
   humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
   print (temperature, humidity)
+  print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
   return
   
 def read_wind():

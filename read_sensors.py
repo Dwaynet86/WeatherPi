31c5712635ebc5bc5649eca558ae311e8210babf
Dwayne Truex <51888733+Dwaynet86@users.noreply.github.com>
2019-07-10 22:06:05 -0500

    Update read_sensors.py

--- read_sensors.py
+++ read_sensors.py
@@ -9,26 +9,19 @@ from gpiozero import LightSensor
 
 def read_temperature():
   sensor = Adafruit_DHT.DHT22
-  pin = 4
-  humidity_raw, temperature_raw = Adafruit_DHT.read_retry(sensor, pin)
+  #pin = 4
+  humidity_raw, temperature_raw = Adafruit_DHT.read_retry(sensor, temperature_pin)
   if humidity_raw is not None and temperature_raw is not None:
     # Convert to F from C and drop excess after decimal
-    temperature_new = round(temperature_raw * 1.8 + 32, 1)
-    humidity_new = round(humidity_raw, 1)
-    
-    #print('Temp={0:0.1f}*F  Humidity={1:0.1f}%'.format(temperature_new, humidity_new)) # for demo only
-    
-    
-    
+    temperature_new = round(temperature_raw * 1.8 + 32, 1) # Convert to farenheit then round value to 1 decimal place
+    humidity_new = round(humidity_raw, 1) # Round value to 1 decimal place
   else:
     print('Failed to get reading from DHT22. Using previous readings.')
   return (humidity_new, temperature_new)
 
 def read_light():
-  ldr = LightSensor(17)  # alter if using a different pin
-  print(ldr)
-  
-  
+  ldr = LightSensor(light_pin)  # alter if using a different pin
+   
   return (ldr.value)
 
 def read_wind():

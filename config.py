a78ac7201d36f8e3bae78dacd21a20a255f87cc0
Dwayne Truex <51888733+Dwaynet86@users.noreply.github.com>
2019-07-10 21:21:20 -0500

    Update config.py

--- config.py
+++ config.py
@@ -11,3 +11,12 @@ db_host     = "localhost"
 # User Options
 
 read_interval = 30  # Sensor Read Interval Time in Seconds 300 = 5 minutes
+
+
+# define pins used
+
+temperature_pin = 4
+light  _pin = 17
+#windspeed_pin = 
+#winddirection_pin =
+#rain_pin =
